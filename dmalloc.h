b61d5848c7cb5d25eb2cdf233942bfb3a01cfae3
Deva Midhun <admin@coder.is-a.dev>
2026-07-29 15:45:28 +0000

    added pal?

--- dmalloc.h
+++ dmalloc.h
@@ -4,9 +4,12 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define MMAP_THRESHOLD (128 * 1024)
+
 typedef struct Header {
   size_t size;
   struct Header *next;
+  uint8_t is_mmapped;
 } Header;
 
 void *malloc(size_t size);
--- glue.h
+++ glue.h
@@ -1,6 +1,26 @@
 #ifndef GLUE_H
 #define GLUE_H
 
-#define sys_brk 
+#include <stddef.h>
+
+// Allocates clean page(s)
+void *pal_mmap(size_t bytes);
+
+// Releases mapped page(s)
+int pal_munmap(void *addr, size_t bytes);
+
+#ifdef GIMPL
+void *pal_mmap(size_t bytes) {
+    void *ptr = mmap(NULL, bytes, PROT_READ | PROT_WRITE, 
+                     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+                     
+    if (ptr == MAP_FAILED) return NULL;
+    return ptr;
+}
+
+int pal_munmap(void *addr, size_t bytes) {
+    return munmap(addr, bytes);
+}
+#endif
 
 #endif
