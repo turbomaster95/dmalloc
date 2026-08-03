882b7133cd6155c619d79887ec9c893888870ed4
Deva Midhun <admin@coder.is-a.dev>
2026-08-03 15:28:56 +0530

    updated glue.h

--- glue.h
+++ glue.h
@@ -10,6 +10,18 @@ void *pal_mmap(size_t bytes);
 int pal_munmap(void *addr, size_t bytes);
 
 #ifdef GIMPL
+
+#include <sys/types.h>
+void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+int munmap(void *addr, size_t length);
+
+#define MAP_PRIVATE    0x02
+#define MAP_ANON       0x20
+#define MAP_ANONYMOUS  MAP_ANON
+#define MAP_FAILED ((void *) -1)
+#define PROT_READ      1
+#define PROT_WRITE     2
+
 void *pal_mmap(size_t bytes) {
     void *ptr = mmap(NULL, bytes, PROT_READ | PROT_WRITE, 
                      MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
